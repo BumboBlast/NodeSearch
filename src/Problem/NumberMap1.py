55128b1f0780bc32a64f7f0b7717cabadc156e46
John James <john.jameschar@gmail.com>
2025-08-04 22:14:26 -0500

    feat: refactored SolveEightPuzzle into directory

--- src/Problem/NumberMap1.py
+++ src/Problem/NumberMap1.py
@@ -1,20 +1,42 @@
 ''' Number Map 1: Figureate Chain
+    Project Euler 61: https://projecteuler.net/problem=61
 
     Number 8134 maps to numbers that start with "34" or ends with "81"
+    Numbers also have types (which poly function they could be from)
     example:
-        8134 -> [3456, 3412, 5681]
-        3456 -> [8134, 5677]
+        8134 -> [3456, 3412, 5681]          | type      8134 -> [3, 4]
+        3456 -> [8134, 5677]                | type      3456 -> [8]
+    and so on.
+
+
+    A state is a given chain consecutivley connected nodes on the map
+    exmaple:
+        8134, 3456, 5612, 1249
     
-    and so on
+    Initial State could be any single node (chain size 1)
+    example:
+        8134    
+    
+    A solution is a chain of 6 length where the back could map to the front
+    A solution ALSO requires representaiton from all 6 poly funciton types
+    example:
+    Nodes:  8134,   3456,   5612,   1249,   4977,   7781
+    Types:  3,      5,      8,      7,      6,      4
+    
+    Actions:
+        for each node in frontier:
+            append_node()
 '''
 
 from Problem.Problem import Problem
+from collections import deque
 
 class FigurateChain(Problem):
-    def __init__(self, initial_state: str, four_digit_map: dict):
+    def __init__(self, initial_state: deque, node_map: dict, type_map: dict):
         super().__init__()
-        self.initial_state: str = initial_state
-        self.map :dict = four_digit_map
+        self.initial_state: deque = initial_state
+        self.node_map : dict = node_map
+        self.node_type_map : dict = type_map 
 
     @staticmethod
     def step_cost(state: str, action: function) -> int:
@@ -35,12 +57,13 @@ class FigurateChain(Problem):
         return action(state)
     @staticmethod
     def is_solvable() -> bool:
+        ''' assume this problem is solvable '''
         return True
     def get_actions(self, state: str) -> list:
         ''' return list of functions that evaluate to the next step
         '''
         action_list : list = list()
-        for action_result in self.map[state]:
+        for action_result in self.node_map[state]:
             def f(state: str) -> str:
                 return action_result
             action_list.append(f)
