ded66daae4a49f584ecd81d2ae0286a403ff2d41
BumboBlast <82180559+BumboBlast@users.noreply.github.com>
2026-01-13 15:03:33 -0600

    feat: MemoryTracking does not use psutil for now

--- src/MemoryTracking.py
+++ src/MemoryTracking.py
@@ -1,27 +1,31 @@
 import time
 import os
-import psutil
+# import psutil
 
 def elapsed_since(start):
     return time.strftime('%H:%M:%S', time.gmtime(time.time() - start))
 
-def get_process_memory():
-    process = psutil.Process(os.getpid())
-    return process.memory_info().rss
+# def get_process_memory():
+    # process = psutil.Process(os.getpid())
+    # return process.memory_info().rss
 
 def track(func):
     def wrapper(*args, **kwargs):
-        mem_before = get_process_memory()
+        # mem_before = get_process_memory()
+        mem_before = 0
         start = time.time()
         result = func(*args, **kwargs)
         elapsed_time = elapsed_since(start)
-        mem_after = get_process_memory()
-        print('---------------------')
-        print('{}:\n\tmemory before: {:,} Bytes\n\tmemory after: {:,} Bytes\n\tmemory consumed: {:,} Bytes\n\texec time: {}s'.format(
-            func.__name__,
-            mem_before, mem_after, mem_after - mem_before,
-            elapsed_time
-        ))
+        # mem_after = get_process_memory()
+        mem_after = 0
+        print("---------------------")
+        print(
+            f"{func.__name__}:\n",
+            f"\tmemory before: {mem_before} Bytes\n",
+            f"\tmemory after: {mem_after} Bytes\n",
+            f"\tmemory consumed: {mem_after - mem_before} Bytes\n",
+            f"\texec time: {elapsed_time}s"
+        )
         print('---------------------')
         return result
     return wrapper
